cfbfffaf94aaac3e7389f82768bd5beafd3a8844
Sait Can Tunç <tuncsaitcan@gmail.com>
2021-02-03 14:53:54 +0300

    Add project files.

---
+++ Business/Concrete/CarManager.cs
@@ -0,0 +1,51 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CarManager : ICarService
+    {
+        ICarDal _carDal;
+
+        public CarManager(ICarDal carDal)
+        {
+            _carDal = carDal;
+        }
+
+        public void Add(Car car)
+        {
+            _carDal.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            _carDal.Delete(car);
+        }
+
+        public List<Car> GetAll()
+        {
+            //iş Kodları
+            //ICarDal interfacesindeki GetAll metodunu kullanabilmek için
+            return _carDal.GetAll();
+        }
+
+        public List<Car> GetAllByCategory(int categoryId)
+        {
+            return _carDal.GetAllByCategory(categoryId);
+        }
+
+        public List<Car> GetById(int id)
+        {
+            return _carDal.GetById(id);
+        }
+
+        public void Update(Car car)
+        {
+            _carDal.Update(car);
+        }
+    }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,45 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.Description);
+            }
+
+            carManager.Add(new Car { Id = 4,CategoryId = 2, BrandId = 2, ColorId = 4, ModelYear = 2018, DailyPrice = 1300, Description = "Nissan GTR r35 2018 Black Edition" });
+
+            Console.WriteLine("ADD -------------------");
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.Description);
+            }
+
+            carManager.Update(new Car { Id = 3, DailyPrice = 1100, Description = "Nissan GTR r35 2017 Nismo--ZAMLANDI" });
+
+            Console.WriteLine("UPDATE -------------------");
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.Description);
+            }
+
+            carManager.Delete(new Car { Id = 4 });
+
+            Console.WriteLine("DELETE -------------------");
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.Description);
+            }
+
+        }
+    }
+}
